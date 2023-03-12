b87ec7a73c7a3bced407edde55d58382e29845bd
electron13 <106238433+electron13@users.noreply.github.com>
2023-03-12 19:50:12 +0800

    Add files via upload

---
+++ Sum Over Zero.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define ll long long
+using namespace std;
+
+ll dp(ll N, ll a[]) {
+    vector<ll> v;
+    ll sum = 0;
+    int j = 0;
+    for (int i = 0; i < N; i++) {
+        if (a[i] > 0) {
+            j = i;
+            v.push_back(j + 1);
+            break;
+        }
+    }
+    for (int i = j; i < N; i++) {
+        sum += a[i];
+        if (sum >= 0 && i>j) {
+            v.push_back(i + 1);
+        }
+    }
+
+    if (v.size() >= 2) {
+        return v[v.size() - 1] - v[0] + 1;
+    }
+    else if (v.empty() == true) {
+        return 0;
+    }
+    else if (v.size() == 1) {
+        return 0;
+    }
+
+}
+
+
+void solve() {
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++) {
+        int inp;
+        cin >> inp;
+        a[i] = inp;
+    }
+    cout << dp(n, a) << " ";
+
+
+}
+
+int main() {
+    int t = 1;
+    for (int u; u < t; u++) {
+        solve();
+    }
+}
